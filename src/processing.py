b7f6428ca100a8fdb8cb9cb65a9f5466f4e3fefa
Aleksandr <feat9999@yandex.ru>
2025-02-06 15:55:31 +0400

    new function

--- src/processing.py
+++ src/processing.py
@@ -1,20 +1,29 @@
 from typing import Dict, List
 
+
 def filter_by_state(last_dict: List[Dict], value_key: str = 'EXECUTED') -> List[Dict]:
     """функция которая принимает список словарей и опционально значение для ключа
 (state) и возвращает новый список словарей, содержащий только те словари, у которых ключ
 (state) соответствует указанному значению"""
-    new_list =[]
+    new_list = []
     for i in last_dict:
         if i['state'] == value_key:
             new_list.append(i)
     return new_list
 
 
-"""Проверка функций"""
-#my_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-#           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-#           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-#           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+def sort_by_date(list_dict: List[Dict], sort_key: bool = True) -> List[Dict]:
+    """функция которая принимает список словарей и необязательный параметр, задающий порядок
+    сортировки (по умолчанию — убывание) и возвращает новый список, отсортированный по дате (date)"""
+    sort_list = sorted(list_dict, key=lambda x: x['date'], reverse=sort_key)
+    return sort_list
 
-#print(filter_by_state(my_list))
+
+"""Проверка функций"""
+my_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+print(filter_by_state(my_list))
+print(sort_by_date(my_list))
