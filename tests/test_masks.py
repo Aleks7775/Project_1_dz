97708b7932198d689298f06734389b070aad217f
Aleksandr <feat9999@yandex.ru>
2025-02-18 22:00:38 +0400

    add file new test, revision project, file readme, new folder htmlcov

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -6,12 +6,12 @@ from src.masks import get_mask_account, get_mask_card_number
 @pytest.mark.parametrize("num_cards, expected_mask", [
     ('7000792289606361', '7000 79** **** 6361'),
     ('1234567891234567', '1234 56** **** 4567')])
-def test_mask_card_number(num_cards, expected_mask):
+def test_mask_card_number(num_cards: str, expected_mask: str) -> None:
     """Тестирование правильности маскирования номера карты"""
     assert get_mask_card_number(num_cards) == expected_mask
 
 
-def test_mask_card_number_for_empty(bank_product):
+def test_mask_card_number_for_empty(bank_product: list) -> None:
     """Проверка работы функции get_mask_card_number на различных входных форматах с использованием фикстуры"""
     for num_card in bank_product:
         assert get_mask_card_number(num_card) == "некорректный номер карты"
@@ -20,12 +20,12 @@ def test_mask_card_number_for_empty(bank_product):
 @pytest.mark.parametrize("num_account, expected_mask", [
     ('73654108430135874305', '**4305'),
     ('12345678912345678912', '**8912')])
-def test_mask_account(num_account, expected_mask):
+def test_mask_account(num_account: str, expected_mask: str) -> None:
     """Тестирование правильности маскирования номера счёта"""
     assert get_mask_account(num_account) == expected_mask
 
 
-def test_mask_account_for_empty(bank_product):
+def test_mask_account_for_empty(bank_product: list) -> None:
     """Проверка работы функции get_mask_account на различных входных форматах с использованием фикстуры"""
     for num_account in bank_product:
         assert get_mask_account(num_account) == "некорректный номер счёта"
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -9,12 +9,12 @@ from src.widget import get_date, mask_account_card
     ('Счет 73654108430135874305', 'Счет **4305'),
     ('Счет 35383033474447895560', 'Счет **5560')
 ])
-def test_mask_account_new_card(bank_prod, mask_bank_prod):
+def test_mask_account_new_card(bank_prod: str, mask_bank_prod: str) -> None:
     """Тестирование правильности маскирования номера карты и счета"""
     assert mask_account_card(bank_prod) == mask_bank_prod
 
 
-def test_mask_card_number_for_empty(bank_product):
+def test_mask_card_number_for_empty(bank_product: list) -> None:
     """Проверка работы функции mask_account_card на различных входных форматах с использованием фикстуры"""
     for bank_prod in bank_product:
         assert mask_account_card(bank_prod) == "некорректный номер карты или счета"
@@ -23,12 +23,12 @@ def test_mask_card_number_for_empty(bank_product):
 @pytest.mark.parametrize("data_entry, data_out", [
     ('2024-03-11T02:26:18.671407', '11.03.2024'),
     ('2010-05-12T02:26:18.671407', '12.05.2010')])
-def test_get_date(data_entry, data_out):
+def test_get_date(data_entry: str, data_out: str) -> None:
     """Тестирование правильности преобразования даты"""
     assert get_date(data_entry) == data_out
 
 
-def test_get_date_format(data_get_format):
+def test_get_date_format(data_get_format: str) -> None:
     """Тестирование правильности преобразования даты с использованием фикстуры"""
     for data in data_get_format:
         assert get_date(data_get_format) == "Некорректный формат даты"
