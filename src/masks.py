63832422d696b85a7cdc5ce84192f9aa05094007
Aleksandr <feat9999@yandex.ru>
2025-03-10 22:42:12 +0400

    add logging file "masks" and "utils"

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,25 @@
+import logging
+
+logger = logging.getLogger("masks")
+
+file_handler = logging.FileHandler("/home/aleksandr/PycharmProjects/PythonProject2/logs/masks_log.log", 'w')
+file_formatter = logging.Formatter('%(asctime)s -  %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
 def get_mask_card_number(mask_card: str) -> str:
     """Функция маскировки номера банковской карты"""
     if isinstance(mask_card, str):
         if len(mask_card) == 16:
             str_mask_card = str(mask_card)
-            return f"{str_mask_card[:4]} {str_mask_card[4:6] + '**'} **** {str_mask_card[12:]}"
-    return "некорректный номер карты"
+            card_mask = f"{str_mask_card[:4]} {str_mask_card[4:6] + '**'} **** {str_mask_card[12:]}"
+            logger.info(f"Успешная маскировка банковской карты: {card_mask}")
+            return card_mask
+    error_message_msk = "некорректный номер карты"
+    logger.error(error_message_msk)
+    return error_message_msk
 
 
 def get_mask_account(mask_account: str) -> str:
@@ -12,5 +27,9 @@ def get_mask_account(mask_account: str) -> str:
     if isinstance(mask_account, str):
         if len(mask_account) == 20:
             str_mask_account = str(mask_account)
-            return f"**{str_mask_account[-4:]}"
-    return "некорректный номер счёта"
+            account_mask = f"**{str_mask_account[-4:]}"
+            logger.info(f"Успешная маскировка банковского счета: {account_mask}")
+            return account_mask
+    error_message_acc = "некорректный номер счёта"
+    logger.error(error_message_acc)
+    return error_message_acc
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,12 @@
 import json
+import logging
+
+logger = logging.getLogger("utils")
+file_handler = logging.FileHandler("/home/aleksandr/PycharmProjects/PythonProject2/logs/utils_log.log", 'w')
+file_formatter = logging.Formatter('%(asctime)s -  %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def json_file(file):
@@ -8,6 +16,8 @@ def json_file(file):
     try:
         with open(file, "r", encoding="utf-8") as operations:
             transactions_data = json.load(operations)
+            logger.info("файл открыт")
             return transactions_data
     except (json.JSONDecodeError, FileNotFoundError):
+        logger.error("отсутствует файл")
         return []
